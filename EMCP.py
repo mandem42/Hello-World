7ec2f36507e04efe97e0cd8aa928d7b2991b9527
mandem42 <104916886+mandem42@users.noreply.github.com>
2022-05-04 11:30:14 -0400

    Update EMCP.py

--- EMCP.py
+++ EMCP.py
@@ -1,7 +1,14 @@
-class Myclass:
-    student_name = "John"
-    student_last = "Python"
-    student_age = 15
+n1 = input ("What is number 1")
+n2 = input ("What is number 2")
+n3 = input ("What is number 3")
 
-test_var = Myclass()
-print(test_var.student_name)
+if n1 >n2:
+    if n1>n3:
+        print ("number 1 is the biggest one")
+if n2 > n3:
+    if n2 > n1:
+        print ("number 2 is bigger than all of them")
+else:
+    if n3>n2:
+        if n3>n1:
+            print ("number 3 is bigger than all of them")
