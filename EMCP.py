52f6da9f6b812c5712fa7dbd0d0d50714b4d92ec
mandem42 <104916886+mandem42@users.noreply.github.com>
2022-05-06 08:58:00 -0400

    Add files via upload

--- EMCP.py
+++ EMCP.py
@@ -1,14 +1,125 @@
-n1 = input ("What is number 1")
-n2 = input ("What is number 2")
-n3 = input ("What is number 3")
+import turtle
 
-if n1 >n2:
-    if n1>n3:
-        print ("number 1 is the biggest one")
-if n2 > n3:
-    if n2 > n1:
-        print ("number 2 is bigger than all of them")
-else:
-    if n3>n2:
-        if n3>n1:
-            print ("number 3 is bigger than all of them")
+#Window Setup
+
+wn = turtle.Screen()
+wn.title("Pong Game by Echo")
+wn.bgcolor("dark gray")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("black")
+paddle_a.shapesize(stretch_wid = 5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("black")
+paddle_b.shapesize(stretch_wid = 5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("black")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 0.2
+ball.dy = 0.2
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0 Player B: 0", align= "center", font= ("Verdana", 24, "normal"))
+
+#Score
+score_a = 0
+score_b = 0
+
+#Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y = y + 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+        y = paddle_a.ycor()
+        y = y - 20
+        paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y = y + 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y = y - 20
+    paddle_b.sety(y)
+
+#Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+
+#Main game loop
+while True:
+    wn.update()
+    #border checking
+  
+    #Top and Bottom
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy = ball.dy * -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy = ball.dy * -1
+    
+    #Right and Left
+    if ball.xcor() > 350:
+        score_a = score_a + 1
+        #print("player A: " + str(score_a) + "Player B: " + str(score_b))
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align = "center", font = ("Verdana", 24, "normal"))
+        ball.goto(0,0)
+        ball.dx = ball.dx * -1
+    
+    
+    if ball.xcor() < -350:
+        score_b = score_b + 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align = "center", font = ("Verdana", 24, "normal"))
+        ball.goto(0,0)
+        ball.dx = ball.dx * -1
+    
+    
+    #Move the Ball
+    ball.setx(ball.xcor()+ ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Paddle and Ball collisions
+    if (ball.xcor() < -340 and ball.xcor() > -350) and ball.ycor() < paddle_a.ycor()+50 > paddle_a.ycor()-50:
+        ball.setx(-340)
+        ball.dx = ball.dx * -1
+    elif ball.xcor() > 340 and ball.xcor() <350 and ball.ycor() < paddle_b.ycor()+50 and ball.ycor() > paddle_b.ycor()-50:
+        ball.setx(340)
+        ball.dx = ball.dx * -1
